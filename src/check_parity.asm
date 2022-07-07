f2b708c85c560126e79d95f7fc24d9052ae0c7cb
evillianester <evillianester@gmail.com>
2022-07-07 17:12:56 +0600

    Added check_parity.asm file

---
+++ src/check_parity.asm
@@ -0,0 +1,52 @@
+section .data
+   askMsg db 'Enter a digit: '
+   lenAskMsg equ $ - askMsg
+
+    oddMsg db 'The digit is odd!', 0xA
+    lenOddMsg equ $ - oddMsg
+
+    evenMsg db 'The digit is even!', 0xA
+    lenEvenMsg equ $ - evenMsg
+
+section .bss
+    digit resb 1
+
+section .text
+    global _start
+
+_start:
+    mov edx, lenAskMsg
+    mov ecx, askMsg
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+
+    mov edx, 1
+    mov ecx, digit
+    mov ebx, 2
+    mov eax, 3
+    int 0x80
+
+    mov ax, [digit]
+
+    and ax, 1
+    jz even
+
+    mov edx, lenOddMsg
+    mov ecx, oddMsg
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+    jmp exit
+
+even:
+    mov edx, lenEvenMsg
+    mov ecx, evenMsg
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+    
+exit:
+    mov eax, 1
+    int 0x80
+
