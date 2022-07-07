2c18e6ed892ffe142e7f52b58a3e69ca31b25d63
evillianester <evillianester@gmail.com>
2022-07-07 19:20:04 +0600

    Added encrypt.asm file

---
+++ src/encrypt.asm
@@ -0,0 +1,34 @@
+section .data
+    string db 'password', 0
+    len equ $ - string
+
+section .bss
+    enc_string resb 10
+
+section .text
+    global _start
+
+_start:
+    mov ecx, len
+    mov esi, string
+    mov edi, enc_string
+
+l1:
+    lodsb
+    add al, 2
+    stosb
+    loop l1
+
+    cld
+
+    rep movsb
+
+    mov edx, 10
+    mov ecx, enc_string
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+
+    mov eax, 1
+    int 0x80
+
